49485d3da87aace156aa0b5b88c5f53762784a67
ArielN98 <aribuzzz98@gmail.com>
2019-06-02 21:26:15 -0300

    Scroll tipo tinder + tragosBase

---
+++ Android/BarmanIOT/app/src/main/java/com/example/myapplication/adapters/TragosPagerAdapter.java
@@ -0,0 +1,24 @@
+package com.example.myapplication.adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.myapplication.R;
+import com.example.myapplication.model.Trago;
+
+public class TragosAdapter extends PagerAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+        return false;
+    }
+}
